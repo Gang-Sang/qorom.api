02dd8ef9d6c5dcc64d834336f8c2382b7f67aee8
Gang-Sang <sangx001@gmail.com>
2023-05-24 22:56:26 -0500

    data repository structure

---
+++ src/qorom.api.infrastructure/mongo/dataModel/Forum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qorom.api.infrastructure.mongo.dataModel
+{
+    internal class Forum
+    {
+    }
+}
